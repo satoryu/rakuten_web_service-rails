7801cac92e4d9c93748a1b2f14a419fc5472c923
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-10-31 23:10:42 +0900

    Add dummy Rails application
    Just performed the following command:
      $ rails _4.2.5_ new ./spec/dummy_app --skip-gemfile -G -d sqlite3 -J -T

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../dummy_app/config/environment', __FILE__)
 require 'rakuten_web_service/rails'
