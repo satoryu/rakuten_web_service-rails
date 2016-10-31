8db1339fb9cf8963c793856ddeecad1174276a90
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-10-31 09:12:32 +0900

    Initial Commit

---
+++ lib/rakuten_web_service/rails.rb
@@ -0,0 +1,7 @@
+require "rakuten_web_service/rails/version"
+
+module RakutenWebService
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/rakuten_web_service/rails/version.rb
@@ -0,0 +1,5 @@
+module RakutenWebService
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
