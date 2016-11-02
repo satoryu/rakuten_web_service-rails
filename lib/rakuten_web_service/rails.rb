3e8453c01efcd658e2973d65012487e2ff642887
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-11-02 13:06:00 +0900

    Loads the configuration file in YAML in booting a Rails app

--- lib/rakuten_web_service/rails.rb
+++ lib/rakuten_web_service/rails.rb
@@ -1,7 +1,16 @@
+require 'rakuten_web_service'
 require "rakuten_web_service/rails/version"
 require 'rails'
 
 module RakutenWebService
   class Railtie < ::Rails::Railtie
+    initializer "rakuten_web_service.load_configuration" do
+      config_path = ::Rails.root.join('config', 'rakuten_web_service.yml')
+      config = YAML.load_file(config_path)[::Rails.env]
+      RakutenWebService.configure do |c|
+        c.application_id = config['application_id']
+        c.affiliate_id = config['affiliate_id']
+      end
+    end
   end
 end
--- spec/rakuten_web_service/rails_spec.rb
+++ spec/rakuten_web_service/rails_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe RakutenWebService::Railtie do
+  describe RakutenWebService::Configuration do
+    subject { RakutenWebService.configuration }
+
+    it "has the configurations for TEST env" do
+      expect(subject.application_id).to eq 'test_application_id'
+      expect(subject.affiliate_id).to eq 'test_affiliate_id'
+    end
+  end
 end
