883032271543afcb1cade38b1849836392b0bf15
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-11-03 12:17:52 +0900

    RWS::Railtie.load_configuration support ERB-based configuration file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,18 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rakuten_web_service/rails'
+
+RSpec.configure do |config|
+  config.around(:all) do |example|
+     if env = example.metadata[:env]
+       env.each do |k, v|
+         ENV[k.to_s.upcase] = v.to_s
+       end
+
+       example.run
+
+       env.keys { |k| ENV.delete k }
+     else
+       example.run
+     end
+  end
+end
