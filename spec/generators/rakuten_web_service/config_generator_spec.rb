c96b46ba7d9d1b6cc888a0fcf1a844f26e8e4563
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-12-20 13:46:31 +0900

    Define a test to check the generator generates new configuration file in YAML

--- spec/generators/rakuten_web_service/config_generator_spec.rb
+++ spec/generators/rakuten_web_service/config_generator_spec.rb
@@ -3,7 +3,17 @@ require 'spec_helper'
 require 'generators/rakuten_web_service/config_generator'
 
 describe RakutenWebService::ConfigGenerator, type: :generator do
+  destination File.expand_path("./../../../tmp", __FILE__)
+
   before do
+    prepare_destination
+
+    run_generator
   end
 
+  describe "generated configuration file" do
+    subject { file('config/rakuten_web_service.yml') }
+
+    it { is_expected.to exist }
+  end
 end
