798b99e431179b2651490991f4ccc414def2884d
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2017-09-18 11:33:26 +0900

    Send coverage report after builds

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+if ENV['CI']
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 ENV['RAILS_ENV'] = 'test'
